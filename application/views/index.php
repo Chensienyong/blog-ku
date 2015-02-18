83ee47f3bd7c8dc791ede80d4e1fd14aeeb74b1d
Jeffry Tandiono <ashxcellent_kingzr@yahoo.com>
2015-02-18 17:31:41 +0700

    website under construction

---
+++ application/views/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Website under construction!</title>
+	<style type="text/css" media="screen">
+		img {
+			width: 80%;
+		}
+		#body {
+			background-color: black;
+		}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<img src="assets/images/under_construction.png">
+</div>
+
+</body>
+</html>
